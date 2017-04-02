911164a8b3bf0ef0e7b697df677ce4329fd6570a
Tiago Ferreira <ferreiratiago@outlook.com>
2017-04-02 10:55:24 +0100

    Add fromIndex playground

--- array.js
+++ array.js
@@ -1,6 +1,6 @@
 /** Array **/
 
-// Array.prototype.includes
+// Array.prototype.includes(searhElement, fromIndex)
 // Determines if an array includes the specified element.
 
 [].includes()                   // false
@@ -11,3 +11,22 @@
 [1,2,3].includes(2)             // true
 ['foo','bar'].includes('foo')   // true
 [undefined].includes(undefined) // true
+
+// fromIndex >= 0
+['foo','bar','zed'].includes('foo')    // true - fromIndex 0 by default
+['foo','bar','zed'].includes('foo',1)  // false
+['foo','bar','zed'].includes('bar',1)  // true
+// If the fromIndex is greater or equal to the array size,
+// the array is not searched and false is returned.
+['foo','bar','zed'].includes('bar',10) // false
+
+// fromIndex <= 0
+// When fromIndex is negative a computed index is calculated to be used.
+// If the computed index if less than 0, the intire array is search.
+// The computation goes like this:
+// Computed index = Array length + fromIndex
+// e.g.
+var array = ['a','b','c','d'] // length = 4
+array.includes('b', -3)  // computed index = 4 + (-3) = 1,   result = true
+array.includes('b', -1)  // computed index = 4 + (-1) = 3,   result = false
+array.includes('b', -10) // computed index = 4 + (-10) = -6, result = false
