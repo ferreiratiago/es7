78f336689d5b63eee9ca3da560a33fee8d54b8fd
Tiago Ferreira <ferreiratiago@outlook.com>
2017-04-03 17:01:16 +0100

    Add exponentiation

---
+++ exponentiation.js
@@ -0,0 +1,10 @@
+/** Exponentiation **/
+
+// The exponentiation operator is represented by ** (double asterisk).
+
+// x ** y (aka Math.pow(x,y))
+2 ** 2   // 4
+2 ** 'a' // NaN
+
+var e = 2
+e **= 2  // 4
